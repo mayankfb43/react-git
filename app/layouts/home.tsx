493e944fbf00d5d7f80dc32f4d28ae962d936ab6
mbahuguna <mbahuguna@contractors.medallia.com>
2025-02-18 16:33:24 +0530

    initial project commit

---
+++ app/layouts/home.tsx
@@ -0,0 +1,9 @@
+import { Outlet } from "react-router";
+export default function Home() {
+  return (
+    <>
+      <h1>Home</h1>
+      <Outlet />
+    </>
+  );
+}
--- app/routes.ts
+++ app/routes.ts
@@ -1,3 +1,11 @@
 import { type RouteConfig, index } from "@react-router/dev/routes";
 
-export default [index("routes/home.tsx")] satisfies RouteConfig;
+import { route, layout } from "@react-router/dev/routes";
+
+export default [
+  layout("layouts/home.tsx", [
+    index("routes/welcome.tsx"),
+    route("contact", "routes/contactForm.tsx"),
+    route("sidebar", "routes/sidebar.tsx"),
+  ]),
+] satisfies RouteConfig;
